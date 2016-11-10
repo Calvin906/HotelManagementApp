4003ec36d86f42bd3d08eefdea2cf55a66006c9b
FMock <FMock@users.noreply.github.com>
2016-11-10 07:30:38 -0800

    Made HotelQueries class and pushed DB Schema

---
+++ HotelQueries.java
@@ -0,0 +1,49 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.ArrayList;
+
+public class HotelQueries {
+	
+	   private static final String URL = "jdbc:mysql://localhost/employee_list";
+	   private static final String USERNAME = "root";
+	   private static final String PASSWORD = "password";
+
+	   private Connection connection; // manages connection
+	   private PreparedStatement selectAllCustomers; 
+	   private PreparedStatement selectCustomerByLastName; 
+	   private PreparedStatement insertNewCustomer;
+	   
+	   // constructor
+	   public HotelQueries()
+	   {
+	      try 
+	      {
+	         connection = 
+	            DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+	         // create query that selects all entries in the Customer table
+	         selectAllCustomers = 
+	            connection.prepareStatement("SELECT * FROM customer");
+	         
+	         // create query that selects entries with a specific last name
+	         selectCustomerByLastName = connection.prepareStatement(
+	            "SELECT * FROM customer WHERE cName = ?");
+	         
+	         // create insert that adds a new entry into the database
+	         insertNewCustomer = connection.prepareStatement(
+	            "INSERT INTO customer " + 
+	            "(cName, email, phone) " + 
+	            "VALUES (?, ?, ?)");
+	      }
+	      catch (SQLException sqlException)
+	      {
+	         sqlException.printStackTrace();
+	         System.exit(1);
+	      }
+	   } // end HotelQueries constructor
+
+}
